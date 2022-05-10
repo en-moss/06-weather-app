f519a1dbc4307697531e027cc461a0772d303d9a
ian.moss <ianmsieg@gmail.com>
2022-05-09 22:24:59 -0500

    working on populating the forecast days

--- Assets/script.js
+++ Assets/script.js
@@ -8,10 +8,14 @@ let apiKey = 'c2472aea17954013d40705840bcbffd4';
 
 let latLonBase = 'https://api.openweathermap.org/geo/1.0/direct?q=';
 let cityBaseURL = 'https://api.openweathermap.org/data/2.5/onecall?'
+let cityNameURL = 'https://api.openweathermap.org/data/2.5/weather?'
 
 let city = document.getElementById('citySearch');
 let search = document.getElementById('cityBtn');
 let cityList = document.getElementById('cityList');
+
+let curDayBlock = document.getElementById('day0')
+let days = document.querySelectorAll('#day')
 
 search.addEventListener('click', function(e) {
     e.preventDefault()
@@ -37,8 +41,10 @@ search.addEventListener('click', function(e) {
                         let chosenCoords = this.id
                         let lat = data[chosenCoords].lat
                         let lon = data[chosenCoords].lon
-                        let cityURL = cityBaseURL + 'lat=' + lat + '&lon=' + lon + '&units=imperial&exclude=minutely,hourly&appid=' + apiKey
+                        let cityURL = cityBaseURL + 'lat=' + lat + '&lon=' + lon + '&units=imperial&exclude=minutely,hourly&appid=' + apiKey;
+                        let cityName = cityNameURL + 'lat=' + lat + '&lon=' + lon + '&units=imperial&exclude=minutely,hourly&appid=' + apiKey;
                         localStorage.setItem('cityURL', cityURL);
+                        localStorage.setItem('cityName', cityName)
                     })
                 }
             })
@@ -46,21 +52,29 @@ search.addEventListener('click', function(e) {
     })
 })
 
-let dayWeatherArr = []
-let dayTempArr = []
-let dayHumidArr = []
-let dayUvArr = []
-let dayWindArr = []
+fetch(localStorage.getItem('cityName'))
+    .then (function(response) {
+        response.json()
+            .then (function(data) {
+                curDayBlock.innerHTML = '<strong>' + data.name + '</strong>'
+            })
+    })
+
+let dayWeatherArr = [];
+let dayTempArr = [];
+let dayHumidArr = [];
+let dayUvArr = [];
+let dayWindArr = [];
 
 fetch(localStorage.getItem('cityURL'))
     .then (function(response) {
         response.json()
             .then (function(data) {
-                // let curWeather = data.current.weather[0].id
-                // let curTemp = data.current.temp
-                // let curHumid = data.current.humidity
-                // let curUV = data.current.uvi
-                // let curWind = data.current.wind_speed
+                let curWeather = data.current.weather[0].id
+                let curTemp = data.current.temp
+                let curHumid = data.current.humidity
+                let curUV = data.current.uvi
+                let curWind = data.current.wind_speed
 
                 
                 for (let i = 1; i < 5; i++) {
@@ -70,8 +84,13 @@ fetch(localStorage.getItem('cityURL'))
                     dayUvArr.push(data.daily[i].uvi)
                     dayWindArr.push(data.daily[i].wind_speed)
                 }
-                console.log(dayWeatherArr)
-                console.log(dayTempArr)
-                console.log(dayWindArr)
             })
     })
+
+function popDays() {
+    for (var i = 0; i < 4; i++) {
+        days[i].innerHTML = dayTempArr[i];
+        console.log(dayTempArr)
+    }
+}
+popDays();
